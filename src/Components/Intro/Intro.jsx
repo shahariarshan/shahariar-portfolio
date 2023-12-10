f25428d2b311c49cded729d2d5ecf2f3745bf779
unknown <shahariarmohammadhassan@gmail.com>
2023-12-11 01:45:19 +0600

    navigation using raw css

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -4,16 +4,17 @@ import introImg from '../../assets/1-removebg-preview.png'
 import { Link } from 'react-router-dom';
 const Intro = () => {
     return (
-       <section id='intro'>
+       <section id='intro' className=' hero-content flex justify-between flex-col lg:flex-row-reverse'>
+         <div>
+           <img  src={introImg} alt="Profile" className='bg'/>
+           </div>
         <div className='introContent'>
             <span className='hello'>Hello,</span>
             <span className='introText'>I'm <span>Shahariar <br />Junior Frontend Developer</span></span>
         <p>I am a fresher frontend developer</p>
         <Link><button className='btn'>Hire Me</button></Link>
         </div>
-        <div>
-            <img className='bg' src={introImg} alt="" />
-        </div>
+          
        </section>
     );
 };
